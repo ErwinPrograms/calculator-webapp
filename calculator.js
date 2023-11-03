e6f4529a7192167e79d62046dcddd9cd8160d6ae
ErwinPrograms <erwinprograms@gmail.com>
2023-11-03 09:19:15 -0700

    Add events for digits and reset button on the keypad that update the calculator screen

--- calculator.js
+++ calculator.js
@@ -1,1 +1,27 @@
 let screenElement = document.querySelector(".screen")
+
+let allDigitButtons = document.querySelectorAll(".digit")
+
+allDigitButtons.forEach(element => {
+    element.addEventListener("click", function(e) {
+        appendDigit(this.innerHTML)
+    })
+});
+
+let resetButton = document.querySelector(".reset")
+
+resetButton.addEventListener("click", function(e) {
+    screenElement.innerHTML = "0"
+})
+
+function appendDigit(digit) {
+    if (screenElement.innerHTML == "0") {
+        if (digit == "0") {
+            return
+        } else {
+            screenElement.innerHTML = digit
+        }
+    } else {
+        screenElement.innerHTML = screenElement.innerHTML + digit
+    }
+}
